862969c4f48a1503c737fdad19759c82ac2a595d
Ericm911 <164890005+Ericm911@users.noreply.github.com>
2024-06-05 09:38:40 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 --Add a comment here that explains the purpose of the table and sample data being inserted
 CREATE TABLE observations (
     ID SERIAL PRIMARY KEY,
-    SiteID INT,
+    SiteID CHARACTER VARYING,
     ObservationTimeUTC TIMESTAMP,
     Value FLOAT
 );
